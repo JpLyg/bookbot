fd56460a0fcb1feac4c80746b749ad88c4090abd
JpLyg <japjolla@gmail.com>
2025-06-26 19:48:21 -0700

    character count

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from stats import *
 
 def main():
-    frankenstein_book_path = "books/frankenstein.txt"
-    print(word_counter(frankenstein_book_path), "words found in the document")
-    exit()
+    target_book = "books/frankenstein.txt"
+    print(word_counter(target_book),"words found in the document")
+    print(iterate_characters(get_book_text(target_book).lower()))
 
 main()
 
--- stats.py
+++ stats.py
@@ -4,3 +4,14 @@ def get_book_text(path_):
 
 def word_counter(path_):
     return len(get_book_text(path_).split())
+
+def iterate_characters(book_string):
+    letter_dictionary= {}
+    for i in range(97,123):
+        letter_dictionary[chr(i)]=0
+
+    for letter in letter_dictionary:
+        letter_dictionary[letter] = book_string.count(letter)
+    
+    return letter_dictionary.sort()
+    #print(letter_dictionary)
