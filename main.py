ae7e8a90d2f101152490163d9eb0492e2a0d20a9
JpLyg <japjolla@gmail.com>
2025-06-27 16:20:42 -0700

    Boot.Dev CH3 L2: sys Arguments

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from stats import *
 from lessons import *
+import sys
 
 def main():
-    target_book = "books/frankenstein.txt"
-    print_report(target_book)
+    #sys.argv.append('books/frankenstein.txt')
+    #sys.argv.append(input("Enter Book Path: "))
+
+    if len(sys.argv) <=1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    try:
+        print_report(sys.argv[1])
+    except FileNotFoundError:
+        print ("invalid directory")
+
+    
     exit()
 main()
-
+ 
