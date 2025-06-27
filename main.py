761655501ac7ef408bd11cd60b2c146d838c74c7
JpLyg <japjolla@gmail.com>
2025-06-26 18:22:10 -0700

    2nd py commit

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def get_book_text(path):
-    with open(path) as f:
+def get_book_text(path_):
+    with open(path_) as f:
         return f.read()
 
+def word_counter(path_):
+    return len(get_book_text(path_).split())
+    #return "test"
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    frankenstein_book_path = "books/frankenstein.txt"
+    print(word_counter(frankenstein_book_path), "words found in the document")
+
+
+    
+#    print(get_book_text("books/frankenstein.txt"))
  
 main()
 
